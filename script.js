83628827d6815fd033591e04cab28e131021d28f
jc-wilson <jamescallumwilson@protonmail.ch>
2022-01-13 13:41:31 +0000

    changes

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ event.preventDefault();
 let bill = Number(document.getElementById('billInput').value);
 let tip = Number(document.getElementById('percentageInput').value);
 
-let tipAmount = Math.round((bill / (1 / (tip / 100))) * 100) / 100
-let finalBill = tipAmount + bill
+let tipAmount = Math.round((bill / (1 / (tip / 100))) * 100) / 100;
+let finalBill = tipAmount + bill;
  
-let tipValue = document.querySelector('#tipValue')
-let totalBill = document.querySelector('#totalBill')
+let tipValue = document.querySelector('#tipValue');
+let totalBill = document.querySelector('#totalBill');
 
-tipValue.value = tipAmount.toFixed(2)
-totalBill.value = finalBill.toFixed(2)
+tipValue.value = tipAmount.toFixed(2);
+totalBill.value = finalBill.toFixed(2);
 
 };
 

671864c2b6f02ae04b3efdfb7bf2d1da9db47300
jc-wilson <jamescallumwilson@protonmail.ch>
2022-01-14 18:05:26 +0000

    added CSS

--- script.js
+++ script.js
@@ -7,12 +7,15 @@ let tip = Number(document.getElementById('percentageInput').value);
 
 let tipAmount = Math.round((bill / (1 / (tip / 100))) * 100) / 100;
 let finalBill = tipAmount + bill;
- 
-let tipValue = document.querySelector('#tipValue');
-let totalBill = document.querySelector('#totalBill');
 
-tipValue.value = tipAmount.toFixed(2);
-totalBill.value = finalBill.toFixed(2);
+tipValue.value = `£${tipAmount.toFixed(2)}`;
+totalBill.value = `£${finalBill.toFixed(2)}`;
+
+document.getElementById('tipValue').innerHTML = tipValue.value;
+document.getElementById('totalBill').innerHTML = totalBill.value;
 
 };
+
+
+
 

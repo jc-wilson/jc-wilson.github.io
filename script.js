fb3fb9cee0515b440e1189f148e278af8beb837f
jc-wilson <97450109+jc-wilson@users.noreply.github.com>
2025-10-21 20:11:06 +0100

    Add files via upload
    added megas, added type and generation filtering, converted dropdown into buttons for choosing stats in mini game.

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ fetch("pokemon.json").then(response => {
 
     function duplicatesRemoved(p) {
         return pokemonData.filter(
-            (p, index, self) => index === self.findIndex(obj => obj.number === p.number)
+            (p, index, self) => index === self.findIndex(obj => obj.name === p.name)
         )
     };
 
-    const pokemon = duplicatesRemoved();
+    let pokemon = duplicatesRemoved();
 
     let pokemonTypes = ["Normal", "Fire", "Water", "Electric", "Grass", "Ice", "Fighting", "Poison", "Ground", "Flying", "Psychic", "Bug", "Rock", "Ghost", "Dragon", "Dark", "Steel", "Fairy"]
     let pokemonByType = []
@@ -20,18 +20,40 @@ fetch("pokemon.json").then(response => {
         pokemonByType.push(poke)
     }
 
-    console.log("pokemonByType:", pokemonByType)
+    const bstFilterByTypeElement = document.getElementById("filterByType")
+    let bstFilterPokemon = pokemon
+    bstFilterByTypeElement.addEventListener("change", (event) => {
+        if (bstFilterByTypeElement.value != "all") {
+            bstFilterPokemon = pokemonByType[`${bstFilterByTypeElement.value}`]
+        };
+    });
+
+    let pokemonGens = [0, 151, 251, 386, 493, 649, 721, 809, 905, 1025]
+    let pokemonByGen = []
+
+    for (let i = 0; i <= 9; i++) {
+        const discard = pokemonGens[i]
+        const poke = pokemon.filter(p => Number(p.number) <= pokemonGens[i] && Number(p.number) > pokemonGens[i - 1]);
+        pokemonByGen.push(poke)
+    }
+
+    const bstFilterByGenElement = document.getElementById("filterByGen")
+    bstFilterByGenElement.addEventListener("change", (event) => {
+        if (bstFilterByGenElement.value != "all") {
+            bstFilterPokemon = pokemonByGen[`${bstFilterByGenElement.value}`]
+        };
+    });
 
     function randomPokemon() {
-        const r = Math.floor(Math.random() * 1025) + 1;
-        const p = pokemon.find(p => Number(p.number) === r);
+        const r = Math.floor(Math.random() * pokemon.length) + 1;
+        const p = pokemon[r];
         if (p) return p.name;
         return null;
     };
 
     function randomPokemonFull() {
-        const r = Math.floor(Math.random() * 1025) + 1;
-        const p = pokemon.find(p => Number(p.number) === r);
+        const r = Math.floor(Math.random() * bstFilterPokemon.length) + 1;
+        const p = bstFilterPokemon[r]
         if (p) return p;
         return null;
     };
@@ -40,8 +62,8 @@ fetch("pokemon.json").then(response => {
         const result = randomPokemon();
         const output = document.getElementById("result");
         output.textContent = `Your random pokemon is ${result}!`;
-        const imgUrl = `https://img.pokemondb.net/sprites/scarlet-violet/icon/${result.replace(" ", "-").replace("é", "e").replace(".", "").toLowerCase()}.png`
-        const html = `<img src="${imgUrl}"`;
+
+        const imgUrl = `https://img.pokemondb.net/sprites/scarlet-violet/icon/${result.replace(/ /g, "-").replace(/é/g, "e").replace(/\./g, "").replace(/:/g, "").toLowerCase()}.png`
         const container = document.getElementById("image-container");
         container.innerHTML = `<img src="${imgUrl}">`;
     };
@@ -54,20 +76,13 @@ fetch("pokemon.json").then(response => {
         if (p) console.log(`${p.number}: ${p.name} (${p.types.filter(t => t).join(", ")})`);
     };
 
-
     const pokemonChoice = pokemon.filter(p => Number(p.spDef) === 95);
 
-    console.log("pokemon array:", pokemon);
-
-    pokemonChoice.forEach(listFunction);
+    // pokemonChoice.forEach(listFunction);
 
     const input = document.getElementById("input");
 
-    console.log("input is:", input);
-
     window.bstButtonFunction = function () {
-        console.log("Submit was pressed");
-
         let pokeOrder = []
         for (let i = 1; i <= 6; i++) {
             pokeOrder.push(document.getElementById(`poke${i}`).value)
@@ -84,7 +99,6 @@ fetch("pokemon.json").then(response => {
             "Speed": "speed"
         };
 
-        console.log(pokeOrder)
         let pokeValues2 = []
         for (let i = 0; i <= 5; i++) {
             pokeValues2.push(pokemon.find(p => p.name.toLowerCase() === pokeOrder[i].toLowerCase()))
@@ -118,8 +132,6 @@ fetch("pokemon.json").then(response => {
             }
         });
 
-        console.log(bestAssignment)
-
         let valueOutput = []
 
         pokeValues.forEach((p, i) => {
@@ -132,11 +144,6 @@ fetch("pokemon.json").then(response => {
         pokeValues2 = bestAssignment.map(index => pokeValues2[index]);
 
         bestAssignment = bestAssignment.map(index => pokeNames[index]);
-        console.log("pokeNames:", pokeNames);
-        console.log("pokeValues:", pokeValues);
-        console.log("pokeValues2:", pokeValues2);
-        console.log(valueOutput)
-        console.log(bestAssignment)
 
         for (let i = 1; i <= 6; i++) {
             const output = document.getElementById(`result${i}`)
@@ -157,11 +164,6 @@ fetch("pokemon.json").then(response => {
         ;
 
         document.getElementById("valueTotal").textContent = `${bestSum}`
-
-        console.log("Best assignment of Pokémon to stats:", bestAssignment);
-        console.log("Max BST:", bestSum);
-        console.log(`Best total = ${bestSum}`);
-        console.log(pokeValues2.slice(0, 6));
     };
 
     window.randomiseBstPokemonFunction = function () {
@@ -185,8 +187,6 @@ fetch("pokemon.json").then(response => {
                     output.textContent += `${p.name}, `;
                 }
             );
-
-            console.log("Enter was pressed");
         }
     });
 
@@ -194,14 +194,21 @@ fetch("pokemon.json").then(response => {
         document.querySelectorAll("#bstGame td[id^='bstName'], #bstGame td[id^='bstValue'], #bstGame td[id^='bstGameValueTotal']").forEach(td => {
             td.innerHTML = "&nbsp;";
         });
+        const statMap = ["hp", "attack", "defense", "spAtk", "spDef", "speed"]
         document.getElementById("bstGame").reset()
-        const select = document.getElementById("selectStats");
-        for (let option of select.options) {
-            option.disabled = false;
-        }
+        const bstButtonGroup = document.querySelector('.button-group2');
+        const buttons = bstButtonGroup.querySelectorAll('button');
+        buttons.forEach(button => button.disabled = false);
+
         let gameRandomPokemon = []
-        for (let i = 1; i <= 6; i++) {
-            gameRandomPokemon.push(randomPokemonFull())
+        for (let i = 0; i <= 5; i++) {
+            const r = randomPokemonFull()
+            if (gameRandomPokemon.find(t => t.name === r.name)) {
+                gameRandomPokemon.splice(i, 1)
+                i--;
+                continue;
+            }
+            gameRandomPokemon.push(r)
         }
 
         gameRandomPokemon.push(gameRandomPokemon[5])
@@ -209,31 +216,36 @@ fetch("pokemon.json").then(response => {
         let j = 0
         let gameRandomPokemonOutput = document.getElementById("gameRandomPokemon")
         gameRandomPokemonOutput.textContent = `${gameRandomPokemon[0].name}`
-        let imgUrl = `https://img.pokemondb.net/sprites/scarlet-violet/icon/${gameRandomPokemon[0].name.replace(" ", "-").replace("é", "e").replace(".", "").toLowerCase()}.png`
+        let imgUrl = `https://img.pokemondb.net/sprites/scarlet-violet/icon/${gameRandomPokemon[0].name.replace(/ /g, "-").replace(/é/g, "e").replace(/\./g, "").replace(/:/g, "").toLowerCase()}.png`
         const bstImageContainer = document.getElementById("image-container-gameRandomPokemon");
         bstImageContainer.innerHTML = `<img src="${imgUrl}">`;
 
-        console.log("gameRandomPokemon:", gameRandomPokemon)
-        const selectElement = document.getElementById("selectStats")
+        const bstGameButtonGroupElement = document.getElementById("bstGameButtonGroup")
         let userChoice = []
         let userStats = []
-        selectElement.addEventListener("change", (event) => {
-            const disableChoice = selectElement.selectedIndex
-            document.getElementById("selectStats").options[`${disableChoice}`].disabled = true;
+        let userRunningTotal = []
+        bstGameButtonGroupElement.addEventListener("click", (event) => {
+            const disableChoice = event.target
+            disableChoice.disabled = true;
+
             gameRandomPokemonOutput.textContent = `${gameRandomPokemon[j + 1].name}`
-            imgUrl = `https://img.pokemondb.net/sprites/scarlet-violet/icon/${gameRandomPokemon[j + 1].name.replace(" ", "-").replace("é", "e").replace(".", "").toLowerCase()}.png`
+            imgUrl = `https://img.pokemondb.net/sprites/scarlet-violet/icon/${gameRandomPokemon[j + 1].name.replace(/ /g, "-").replace(/é/g, "e").replace(/\./g, "").replace(/:/g, "").toLowerCase()}.png`
             bstImageContainer.innerHTML = `<img src="${imgUrl}">`;
-            userChoice.push(selectElement.value)
+
+            userChoice.push(event.target.value)
             userStats.push(gameRandomPokemon[j][userChoice[j]])
+
+            const userRunningBST = document.getElementById("bstGameValueTotal")
+            userRunningTotal = userStats.reduce((acc, current) => acc + Number(current), 0);
+            userRunningBST.textContent = `${userRunningTotal}`
+
             let bstName = document.getElementById(`bstName${userChoice[j]}`)
             let bstValue = document.getElementById(`bstValue${userChoice[j]}`)
-            console.log("userChoice:", userChoice)
-            console.log("userStats:", userStats)
+
             bstName.textContent = `${gameRandomPokemon[j].name}`
-            bstValue.textContent = `${gameRandomPokemon[j][selectElement.value]}`
+            bstValue.textContent = `${gameRandomPokemon[j][event.target.value]}`
             j++
             if (j === 6) {
-                console.log("game finished")
                 finishGame()
             }
         })
@@ -269,8 +281,6 @@ fetch("pokemon.json").then(response => {
             const obstGameValueTotal = document.getElementById("obstGameValueTotal")
             userBST.textContent = `${sum1}`
             obstGameValueTotal.textContent = `${bestSum}`
-            console.log("bestAssigment:", bestAssignment)
-            const statMap = ["hp", "attack", "defense", "spAtk", "spDef", "speed"]
 
             for (let i = 0; i <= 5; i++) {
                 const obstName = document.getElementById(`obstName${statMap[i]}`)
@@ -292,11 +302,10 @@ fetch("valorant.json").then(response => {
     function randomAgent() {
         const r = Math.floor(Math.random() * 28) + 1;
         if (r === 8) {
-            r = null;
             randomAgent();
+            r = null;
         }
         ;
-        console.log(r)
         const p = agentData.find(p => Number(p["No."]) === r);
         if (p) return p.Agent;
         return null;
@@ -307,8 +316,6 @@ fetch("valorant.json").then(response => {
         const output = document.getElementById("randomAgentResult");
         output.textContent = `Your random agent is ${result}!`;
     }
-
-    console.log(randomAgent())
 });
 
 
